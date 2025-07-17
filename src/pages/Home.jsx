62af3c481e52399f2ea5303f40544239792c7d64
Sampreeth <sampreeth.personal@gmail.com>
2025-07-17 20:00:55 +0530

    Worked on ThemeToggle functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
 
-    return <div className="min-h-screen ">
-        {/* Theme Selection */}
+    return(
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme Selection */}
+            <ThemeToggle/>
+            {/* Background */}
 
-        {/* Background */}
+            {/* NavBar */}
 
-        {/* NavBar */}
-
-        {/* Main */}
+            {/* Main */}
 
 
-        {/* Footer */}
+            {/* Footer */}
 
-    </div>
+        </div>
+    );
 }
